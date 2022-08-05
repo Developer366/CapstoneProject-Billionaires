f8c48fba5dd50dcb958b5c43415ab2d14c3b91a9
Kamil Peza <>
2022-08-05 15:55:28 -0400

    worked on plots

--- GoogleProject_RScript.R
+++ GoogleProject_RScript.R
@@ -3,8 +3,10 @@
 # install & Load Packages
 install.packages("readxl")
 install.packages("ggplot2")
+install.packages("dplyr")
 library(readxl)
 library(ggplot2)
+library(dplyr)
 
 # saving excel file
 forbes_data <- read.csv("forbes_2022_billionaires.csv")
@@ -22,6 +24,8 @@ max(forbes_data['age'], na.rm = TRUE)
 min(forbes_data['age'])
 mean(forbes_data$age, na.rm=TRUE) # returns 64.206
 summary(forbes_data)
+forbes_data$age
+count(forbes_data)
 
 # counting Male & Female Billionaires, also blanks
 females <- sum(forbes_data$gender == 'F')
@@ -36,15 +40,38 @@ e <- ggplot(mpg, aes(cty, hwy))
 ggplot(mpg, aes(displ, hwy, colour = class)) + 
   geom_point()
 
-#plotting age and net worth
+#plotting age and net worth (in billions)
 ggplot(data = forbes_data, mapping = aes(x = age, y = NetWorth)) +
-  geom_point() + geom_point(colour = "blue")
+  geom_point() + geom_point(colour = "blue") + ggtitle("Billionaire's Net Worth (in billions) and Age")
 
-ggplot(data = forbes_data, mapping = aes(x = age)) +
-  geom_bar(colour = "blue")
+#BAR Plot of genders with labels
+gender <- forbes_data[complete.cases(forbes_data), ]
+gender
+
+ggplot(data = forbes_data, mapping = aes(x = gender)) +
+  geom_bar(stat = "count", fill="steelblue") + 
+  geom_text(stat="count", aes(label = after_stat(count)), vjust=1.6, color="white", size=3.5) +
+  theme_minimal() +
+  ggtitle("Number of Female and Male Billionaires") 
+
+# Don't map a variable to y
+
+ggplot(data = forbes_data, aes(x=gender)) +
+  geom_bar(stat="bin", width=0.7, fill="steelblue") +
+  geom_text(aes(label=gender), vjust=1.6, color="white", size=3.5) +
+  theme_minimal()
+
+ggplot(data= forbes_data, aes(x=age, y=NetWorth)) +
+  geom_bar(stat="identity", fill="steelblue")+
+  geom_text(aes(label=age), vjust=-0.3, size=3.5)+
+  theme_minimal()
+
+
+
 
 
 #--------------------------------------------------------------
 # helpful links/ fixing errors
 # http://uc-r.github.io/na_exclude
 # https://stackoverflow.com/questions/38068774/rstudio-suddenly-stopped-showing-plots-in-the-plot-pane
+# https://www.youtube.com/watch?v=HPJn1CMvtmI&t=181s&ab_channel=RProgramming101
