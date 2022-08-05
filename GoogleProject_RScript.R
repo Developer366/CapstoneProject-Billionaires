cf95fa8ab87b92b17c8659d892fb873b18853ca3
Kamil Peza <>
2022-08-05 11:12:55 -0400

    worked on script and plotting

--- GoogleProject_RScript.R
+++ GoogleProject_RScript.R
@@ -1,16 +1,21 @@
-# Kamil Peza Google Capstone Project 06/2022
+# Kamil Peza Google Capstone Project 06/2022 "Forbes Billionaires"
 
 # install & Load Packages
 install.packages("readxl")
-
+install.packages("ggplot2")
 library(readxl)
+library(ggplot2)
 
 # saving excel file
 forbes_data <- read.csv("forbes_2022_billionaires.csv")
 forbes_data
-dim(forbes_data)
+dim(forbes_data) #count the rows and columns 
 
-#looking at the data -------------------------
+#setting the working Directory on pc
+getwd()
+setwd("C:/Users/Polska/Desktop/CapStoneProject")
+
+#---------------------looking at the data-------------------------
 #The max function won't return any values if it encounters the NA values in the process.
 min(forbes_data$age, na.rm = TRUE) 
 max(forbes_data['age'], na.rm = TRUE)
@@ -18,7 +23,7 @@ min(forbes_data['age'])
 mean(forbes_data$age, na.rm=TRUE) # returns 64.206
 summary(forbes_data)
 
-# counting Male,Female Billionaires, also blanks
+# counting Male & Female Billionaires, also blanks
 females <- sum(forbes_data$gender == 'F')
 females
 males <- sum(forbes_data$gender == 'M')
@@ -26,13 +31,20 @@ males
 blanks <- sum(forbes_data$gender == '')
 blanks
 
-barplot(males, names.arg - females)
+#plotting gender data
+e <- ggplot(mpg, aes(cty, hwy))
+ggplot(mpg, aes(displ, hwy, colour = class)) + 
+  geom_point()
+
+#plotting age and net worth
+ggplot(data = forbes_data, mapping = aes(x = age, y = NetWorth)) +
+  geom_point() + geom_point(colour = "blue")
+
+ggplot(data = forbes_data, mapping = aes(x = age)) +
+  geom_bar(colour = "blue")
 
 
-# pie(x)
-# dev.off()
-
-
+#--------------------------------------------------------------
 # helpful links/ fixing errors
 # http://uc-r.github.io/na_exclude
 # https://stackoverflow.com/questions/38068774/rstudio-suddenly-stopped-showing-plots-in-the-plot-pane
